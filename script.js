c3e3f1f18fb53662e23a1198d70f681f4fc09ca9
JembiPower <163330059+JembiPower@users.noreply.github.com>
2025-01-02 18:09:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Carica il file JSON
+fetch('domande.json')
+  .then(response => response.json())
+  .then(data => {
+    // Le domande sono ora nell'array "data"
+    const domande = data;
 
+    // Funzione per mostrare una domanda random
+    function mostraDomanda() {
+      const domandaRandom = domande[Math.floor(Math.random() * domande.length)];
+      // ... codice per mostrare la domanda e le risposte
+    }
+
+    // Chiama la funzione per mostrare la prima domanda
+    mostraDomanda();
+  });
